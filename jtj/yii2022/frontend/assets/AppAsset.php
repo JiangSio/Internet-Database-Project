ee6d6d8619edd5a4f03fc0474c87b9c160500516
G2 OptJiang20 <2499184084@qq.com>
2023-02-01 21:04:16 +0800

    油价分析界面

--- jtj/yii2022/frontend/assets/AppAsset.php
+++ jtj/yii2022/frontend/assets/AppAsset.php
@@ -17,7 +17,7 @@ class AppAsset extends AssetBundle
     public $js = [
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap5\BootstrapAsset',
+        //'yii\web\YiiAsset',
+        //'yii\bootstrap5\BootstrapAsset',
     ];
 }
---
+++ jtj/yii2022/frontend/models/SjfxGasprice.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * 
+ * Team:流浪NK
+ * Coding by JiangTianjia , 2013095
+ * This is a db model for gasprice
+ */
+namespace frontend\models;
+
+use yii\db\ActiveRecord;
+
+class SjfxGasprice extends ActiveRecord
+{
+
+}
